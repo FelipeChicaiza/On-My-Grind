0fe42b7cb3ee07cbe492b019cd21401f8e4b525e
Felipech292 <felipechicaiza47@gmail.com>
2024-10-05 14:15:49 -0400

    Add RecipeChoices component

--- src/Components/BaristaForm.jsx
+++ src/Components/BaristaForm.jsx
@@ -1,5 +1,18 @@
 import React, {Component, useState} from "react";
 const BaristaForm = () => {
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup':'',
+        'blended': '',
+    });
+
+    const ingredients ={
+        'temperature': ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha','vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes','turbo', 'no']
+    }
 
     const onNewDrink = () => {
     
---
+++ src/Components/RecipeChoices.jsx
@@ -0,0 +1,11 @@
+import React, { Component, useEffect, useState } from "react";
+
+const RecipeChoices = () => {
+    return (
+      <div>
+        
+      </div>
+    );
+};
+
+export default RecipeChoices;
